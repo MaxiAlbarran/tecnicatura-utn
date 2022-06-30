9a3c350060d49a203491b9dda026d41c137d7806
MaxiAlbarran <85417813+MaxiAlbarran@users.noreply.github.com>
2022-06-30 16:18:41 -0300

    Update Camiones.c

--- C/Camiones/Camiones.c
+++ C/Camiones/Camiones.c
@@ -1,10 +1,3 @@
-/*Se cuenta con un vector de estructuras con datos de camiones que realizan viajes entre ciudades.
-Construya cuatro funciones que reciban el vector y su tamaño, y realicen lo siguiente:
-a) Permita el ingreso de los datos de 4 camiones y 2 viajes para cada uno.
-b) Muestre en pantalla las patentes, sin repetir. No se debe modificar el orden del vector original.
-c) Muestre en pantalla la patente del camión que más viajes realizó. Para esto modifique el punto a, de manera de poder ingresar algún camión con más viajes y probar este punto c.
-d) Muestre en pantalla todos los viajes, ordenados por fecha. Los que son de la misma fecha se deben ordenar por peso transportado descendente.*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
